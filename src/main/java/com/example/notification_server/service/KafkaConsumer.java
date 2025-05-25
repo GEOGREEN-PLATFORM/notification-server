30492dcf94434b2eac40560cc2aa2f2b47efca01
Dasha <dashavavilova2002@icloud.com>
2025-05-25 22:55:21 +0300

    Add logs

--- src/main/java/com/example/notification_server/service/KafkaConsumer.java
+++ src/main/java/com/example/notification_server/service/KafkaConsumer.java
@@ -2,10 +2,12 @@ package com.example.notification_server.service;
 
 import com.example.notification_server.model.dto.UpdateElementDTO;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class KafkaConsumer {
@@ -17,6 +19,7 @@ public class KafkaConsumer {
             containerFactory = "factory"
     )
     public void listenFinishRegistration(@Payload UpdateElementDTO updateElementDTO) {
+        log.info("Get message {}", updateElementDTO);
         subscriberNotificationService.notifyUsers(updateElementDTO);
     }
 }
